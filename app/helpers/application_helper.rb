06cf7d191db1366f775e8e95f8b735f6ebae3723
Frank <frank@heidjer.info>
2011-10-28 16:52:58 +0200

    Prevent empty flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,12 @@ module ApplicationHelper
   end
 
   def flash_message
-    mapping = {:notice => "info", :error => "error", :success => "success" }
-    content_tag(:div, :class => "alert-message #{mapping[flash.keys.first]}", :"data-alert" => "alert") do
-      concat(content_tag(:a, "x", :class => "close", :href => "#"))
-      concat(content_tag(:p, flash[flash.keys.first]))
+    unless flash.keys.empty?
+      mapping = {:notice => "info", :error => "error", :success => "success" }
+      content_tag(:div, :class => "alert-message #{mapping[flash.keys.first]}", :"data-alert" => "alert") do
+        concat(content_tag(:a, "x", :class => "close", :href => "#"))
+        concat(content_tag(:p, flash[flash.keys.first]))
+      end
     end
   end
 end
