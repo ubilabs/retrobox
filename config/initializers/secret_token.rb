d8b7e5516ff7443ff30538b648d39a8cc4b7806d
Simon Ernst <ernst@ubilabs.net>
2011-10-28 12:46:48 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Retrobox::Application.config.secret_token = 'dbda7326ce08fa8520af1ddb3682ce6d822857e58791f210376c55cab7689a9bba829646ba31c0eecfb05c9bc1e3f7137b4da3ba1a2a99897905e0b77a7ba221'
