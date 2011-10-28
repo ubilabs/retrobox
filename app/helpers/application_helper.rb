7a2fd16b195fa6239f0423d9a196247c2208e4b7
Frank <frank@heidjer.info>
2011-10-28 16:48:37 +0200

    Add twitter-bootstrap styled flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def title
     "The Retrobox"
   end
+
+  def flash_message
+    mapping = {:notice => "info", :error => "error", :success => "success" }
+    content_tag(:div, :class => "alert-message #{mapping[flash.keys.first]}", :"data-alert" => "alert") do
+      concat(content_tag(:a, "x", :class => "close", :href => "#"))
+      concat(content_tag(:p, flash[flash.keys.first]))
+    end
+  end
 end
