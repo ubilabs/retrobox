31ad305ffeb74cf882d72143b2af8a5eb46114f8
Simon Ernst <ernst@ubilabs.net>
2011-10-31 15:58:11 +0100

    Add very basic user specs.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'database' do
+    it { should have_db_column(:first_name).of_type(:string) }
+    it { should have_db_column(:last_name).of_type(:string) }
+  end
+
+  describe 'factory' do
+    it { Factory.build(:user).should be_valid }
+  end
+
+  # describe 'associations' do
+  #   it { should have_many(:notes) }
+  # end
 end
