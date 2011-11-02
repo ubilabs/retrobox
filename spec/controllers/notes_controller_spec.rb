ed4ff5ce42fdf43ed20b53593e5fe4c240ae8c7f
Simon Ernst <ernst@ubilabs.net>
2011-11-02 15:13:00 +0100

    Update notes controller specs.

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -3,7 +3,14 @@ require 'spec_helper'
 describe NotesController do
 
   describe "GET 'index'" do
-    it "returns http success" do
+    it "should not allow anonymous access" do
+      get 'index'
+      response.should redirect_to(root_path)
+    end
+
+    it "should be accessible by signed in user" do
+      sign_in Factory.create(:user)
+
       get 'index'
       response.should be_success
     end
