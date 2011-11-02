cd1f09dcb58feb946f4a6927eb95e66c6fc28262
Simon Ernst <ernst@ubilabs.net>
2011-11-02 18:13:16 +0100

    Do every note related action in the context of the current_user.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,3 +1,9 @@
 class NotesController < InheritedResources::Base
   before_filter { redirect_to root_path unless user_signed_in? }
+
+  protected
+
+  def begin_of_association_chain
+    current_user
+  end
 end
