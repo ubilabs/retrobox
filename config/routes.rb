f7fb32f223780043fe021fcf53032d3e6780a725
Simon Ernst <se@netmute.org>
2011-12-09 10:47:41 +0100

    Add route for admin controller.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Retrobox::Application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
+  resources :admin
   resources :notes, :only => :index
   resources :green_notes do
     opinio
